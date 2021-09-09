f5e067cd627869931365d18800d738fba3ebda8f
abizmo <abizmo@gmail.com>
2021-09-09 22:19:09 +0100

    Learning useEffect & customHooks

--- src/HookApp.js
+++ src/HookApp.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import CounterApp from './components/01-useState/CounterApp'
 import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook'
 import SimpleForm from './components/02-useEffect/SimpleForm'
 
 const HookApp = () => {
   return (
     <div>
+      <FormWithCustomHook />
       <SimpleForm />
       <CounterWithCustomHook />
       <CounterApp />
---
+++ src/components/02-useEffect/FormWithCustomHook.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react'
+import useForm from '../../hooks/useForm';
+
+import './effects.css'
+
+const FormWithCustomHook = () => {
+  const [formState, handleInputChange] = useForm({
+    email: '',
+    name: '',
+    password: '',
+  })
+
+  const { email, name, password } = formState;
+  
+  useEffect(() => {
+    console.log('email')
+  }, [email])
+
+  return (
+    <div className="simple-form">
+      <h3>FormWithCustomHook</h3>
+      <hr />
+
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Tu nombre"
+          autoComplete="off"
+          value={ name }
+          onChange={ handleInputChange }
+        />
+      </div>
+      <div className="input-group mb-3">
+        <input
+          type="email"
+          name="email"
+          className="form-control"
+          placeholder="Tu email"
+          autoComplete="off"
+          value={ email }
+          onChange={ handleInputChange }
+        />
+      </div>
+      <div className="input-group mb-3">
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="Tu password"
+          value={ password }
+          onChange={ handleInputChange }
+        />
+      </div>
+    </div>
+  )
+}
+
+export default FormWithCustomHook
